99c7e289df147da4606b9ac4d3816a2c130aacfd
lee28 <xoals28655@gmail.com>
2024-04-13 22:35:54 +0900

    Initial commit

---
+++ app/src/main/java/com/example/musicalbum/AudioImage.java
@@ -0,0 +1,14 @@
+package com.example.musicalbum;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AudioImage extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.audio_lmage);
+    }
+}
